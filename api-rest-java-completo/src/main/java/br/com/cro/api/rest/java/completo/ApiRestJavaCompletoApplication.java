26e003b9b316771e35d04ed16bfb792256a8c86b
orc1272 <orc1272@gmail.com>
2023-08-05 23:23:57 -0300

    First commit

---
+++ api-rest-java-completo/src/main/java/br/com/cro/api/rest/java/completo/ApiRestJavaCompletoApplication.java
@@ -0,0 +1,15 @@
+package br.com.cro.api.rest.java.completo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestJavaCompletoApplication {
+
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ApiRestJavaCompletoApplication.class, args);
+	}
+
+}
---
+++ api-rest-java-completo/src/test/java/br/com/cro/api/rest/java/completo/ApiRestJavaCompletoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cro.api.rest.java.completo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestJavaCompletoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
